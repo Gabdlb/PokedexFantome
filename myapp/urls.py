28609271db84c58d9b6627b30c8ff6c59f656578
Gabrieldlb <gabriel.delabiche@epsi.fr>
2022-01-18 22:33:22 +0100

    Base de départ

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
-"""PokedexFantome URL Configuration
+"""DjangoProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    #ici c le path principale el le index c par rapport à ma methode donc à changer qd yaura la methode global daffichage
+    path('', views.index, name="index"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render
+import random
+import requests as re
 
-# Create your views here.
+# ptite methode pour test l'api, la c juste affichache des 151 pok en console
+def index(request):
+    url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151'
+    r = re.get(url)
+    response = r.json()
+    for pokemon in response['results']:
+        print(pokemon['name'])
+
+    return render(request, 'myapp/index.html', response)
+
