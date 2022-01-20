7b255125de71b981b31a32e5c82b7e3f61b8ed3e
Hugo Divet <hugo.divet@epsi.fr>
2022-01-20 17:06:47 +0100

    Gestion des équipes fonctionnelles je crois

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class EquipePokemon(models.Model):
+    #NomEquipe = models.CharField(max_length=50)
+    Pokemon1 = models.IntegerField(max_length=5)
+    Pokemon2 = models.IntegerField(max_length=5)
+    Pokemon3 = models.IntegerField(max_length=5)
+    Pokemon4 = models.IntegerField(max_length=5)
+    Pokemon5 = models.IntegerField(max_length=5)
--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 import random
 import requests as re
+from .models import EquipePokemon
 
 # ptite methode pour test l'api, la c juste affichache des 151 pok en console
 def index(request):
@@ -13,3 +14,17 @@ def index(request):
 
     return render(request, 'myapp/index.html', response)
 
+
+def ajouterEquipe(id_pokemon):
+    if EquipePokemon.Pokemon1 is None:
+        EquipePokemon.Pokemon1 = id_pokemon
+    elif EquipePokemon.Pokemon2 is None:
+        EquipePokemon.Pokemon2 = id_pokemon
+    elif EquipePokemon.Pokemon3 is None:
+        EquipePokemon.Pokemon3 = id_pokemon
+    elif EquipePokemon.Pokemon4 is None:
+        EquipePokemon.Pokemon4 = id_pokemon
+    elif EquipePokemon.Pokemon5 is None:
+        EquipePokemon.Pokemon5 = id_pokemon
+    else:
+       return False
